7b8f52096781e9749c50fc63bfc9feca2f89cc44
egenvall <egenvall.kim@gmail.com>
2016-04-19 16:36:48 +0200

    Empty class

---
+++ app/src/main/java/com/github/agiledevgroup2/xpnavigator/ApiHelper.java
@@ -0,0 +1,23 @@
+package com.github.agiledevgroup2.xpnavigator;
+
+public class ApiHelper {
+    private TrelloClient client = TrelloApplication.getTrelloClient();
+
+    public ApiHelper(){
+
+    }
+
+    public void getBoards(){
+
+    }
+
+    public void getCards(){
+
+    }
+
+    public void getLists(){
+
+    }
+
+
+}
